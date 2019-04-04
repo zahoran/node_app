3d362abb6ece419ef534d063c71aae0d4f96074f
Daniel Zahoran <daniel.zahoran@gmail.com>
2019-04-04 19:35:36 +0200

    prepare app for heroku

--- public/js/app.js
+++ public/js/app.js
@@ -3,7 +3,7 @@ console.log('Client side javascript file is loaded!');
 const geoUrl = 'https://api.mapbox.com/geocoding/v5/mapbox.places/' + 'Boston' +
   '.json?access_token=pk.eyJ1IjoiZGFuaWVsemFob3JhbiIsImEiOiJjanUwNW5wcjEweWdqNDNwbjhlbHRrcHptIn0.Xx28DmAeFdSfS_llb3CvDQ&limit=1';
 
-fetch('http://localhost:3000/weather?address=Boston').then((res) => {
+fetch('/weather?address=Boston').then((res) => {
   res.json().then((data) => {
     console.log(data);
   })
